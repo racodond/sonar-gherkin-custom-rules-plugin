0562b310a424006a8d3c8e609817ce4d52a62e1b
David RACODON <david.racodon@gmail.com>
2017-03-02 11:47:27 +0100

    Remove analysis on SonarQube.com

--- travis.sh
+++ travis.sh
@@ -4,6 +4,6 @@ set -euo pipefail
 
 mvn clean install
 
-if [ "$TRAVIS_BRANCH" == "master" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
-  mvn sonar:sonar -Dsonar.host.url=https://sonarqube.com -Dsonar.login=$SONAR_TOKEN
-fi
+#if [ "$TRAVIS_BRANCH" == "master" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
+#  mvn sonar:sonar -Dsonar.host.url=https://sonarqube.com -Dsonar.login=$SONAR_TOKEN
+#fi
